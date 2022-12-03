595cfc260b7e45d4b52d0157b9a68f9afd9f88de
raulfagnerr <raulfagnerr@gmail.com>
2022-12-02 23:12:21 -0300

    Laboratorio 01

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string sb = new string('=', 40);
+
+Console.WriteLine(sb + " Lab 01 " + sb);
+
+
+Console.Write("Qual seu nome:. ");
+string? nome = Console.ReadLine();
+Console.Write("Qual a sua idade:. ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade <= 17)
+{
+    Console.WriteLine("Valor do Ingresso: R$ 30");
+}
+else if (idade <= 59)
+{
+    Console.WriteLine("Valor do Ingresso: R$ 40");
+}
+else {
+    Console.WriteLine("Valor do Ingresso:. R$ 20");
+    }
+
+
+
+
+Console.ReadKey(); 
